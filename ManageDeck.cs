24a3916da73e40baebd82e7f67fe3891dd1c0e52
ewascent@gmail.com <ewascent@gmail.com>
2015-01-19 23:19:09 -0800

    added sorting logic and deck generation

--- ManageDeck.cs
+++ ManageDeck.cs
@@ -3,48 +3,168 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace CardGame
 {
-
+    //Create a helper class to simplify the creation of a list of tuples.
+    //Each card will be represented as a tuple and the list will represent the deck
+    public class TupleList<T1, T2, T3, T4, T5> : List<Tuple<T1, T2, T3, T4,T5>>
+    {
+        public void Add(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5)
+        {
+            Add(new Tuple<T1, T2, T3, T4, T5>(item1, item2, item3, item4, item5));
+        }
+    }
     public class ManageDeck
     {
-             //byte is an economical numeric datatype that stores values up to 32,767
+        List<string> suitType = new List<string>();
+        List<string> cardType = new List<string>();
+        TupleList<byte,string, string, byte, byte?> deck = new TupleList<byte,string, string, byte, byte?>();
+        //byte is an economical numeric datatype that stores values up to 32,767
         //alternateWeight is usually null adn is deisgned for card games that allow one card to have two values like the ace in blackjack
-        public Array makeDeck(out Array decker)
+        public string addCards(byte sortOrder,string cardVal, string suitVal, byte weight, byte? alternateWeight)
         {
-            string[] deck = new string[] { "AC", "AD", "AH", "AS", "KC", "KD", "KH", "KS", "QC", "QD", "QH", "QS", "JC", "JD", "JH", "JS", "10C", "10D", "10H", "10S", "9C", "9D", "9H", "9S", "8C", "8D", "8H", "8S", "7C", "7D", "7H", "7S", "6C", "6D", "6H", "6S", "5C", "5D", "5H", "5S", "4C", "4D", "4H", "4S", "3C", "3D", "3H", "3S", "2C", "2D", "2H", "2S", };
-            decker = deck;
-            return decker;
+            var format = new FormatException();
+            try
+            {
+                suitType.Add("Clubs");
+                suitType.Add("Diamonds");
+                suitType.Add("Hearts");
+                suitType.Add("Spades");
+                cardType.Add("Ace");
+                cardType.Add("King");
+                cardType.Add("Queen");
+                cardType.Add("Jack");
+                cardType.Add("10");
+                cardType.Add("9");
+                cardType.Add("8");
+                cardType.Add("7");
+                cardType.Add("6");
+                cardType.Add("5");
+                cardType.Add("4");
+                cardType.Add("3");
+                cardType.Add("2");
+                if (cardType.Contains(cardVal))
+                {
+                    if (suitType.Contains(suitVal))
+                    {
+                        deck.Add(sortOrder, cardVal, suitVal, weight, alternateWeight);
+                        return "Eric does not know how to return a tuplelist";
+                    }
+                    else throw format;
+                }
+                else throw format;
+            }
+            catch
+            {
+                throw format;
+                //return "Exception 100: Card could not be added";
+            }
         }
 
-
-        public int shuffle(int CardCount)
+        public void shuffleDeck(int CardCount)
         {
-            //http://blog.codinghorror.com/shuffling/
-            var cards = Enumerable.Range(0, 51);
-            var shuffledcards = cards.OrderBy(a => Guid.NewGuid());
-
-            return 1;
-            
+            Console.WriteLine("I am dead to the world. fix me");
         }
-
 
         private static void Main(string[] args)
         {
-            //addCards = new addCards();
+            var make = new ManageDeck();
 
-            //addCards("Ace", "Hearts", 11, 1);
-            //addCards("King", "Hearts", 10, 0);
+            make.addCards(10,"2", "Hearts", 2, null);
+            make.addCards(1,"Ace", "Clubs", 11, 1);
+            make.addCards(2,"10", "Clubs", 10, null);
+            make.addCards(3,"9", "Diamonds", 9, null);
+            make.addCards(2,"King", "Clubs", 10, null);
+            make.addCards(2,"King", "Diamonds", 10, null);
+            make.addCards(10, "2", "Spades", 2, null);
+            make.addCards(2, "King", "Hearts", 10, null);
+            make.addCards(2,"Queen", "Diamonds",10, null);
+            make.addCards(2,"Queen", "Hearts",  10, null);
+            make.addCards(2,"Queen", "Spades",  10, null);
+            make.addCards(6,"6", "Clubs", 6, null);
+            make.addCards(6,"6", "Diamonds", 6, null);
+            make.addCards(2,"King", "Spades", 10, null);
+            make.addCards(2,"Queen", "Clubs", 10, null);
+            make.addCards(2,"Jack", "Clubs", 10, null);
+            make.addCards(2,"Jack", "Diamonds", 10, null);
+            make.addCards(2,"Jack", "Hearts",   10, null);
+            make.addCards(1,"Ace", "Diamonds", 11, 1);
+            make.addCards(1,"Ace", "Hearts", 11, 1);
+            make.addCards(2,"Jack", "Spades", 10, null);
+            make.addCards(2,"10", "Diamonds",   10, null);
+            make.addCards(2,"10", "Hearts",     10, null);
+            make.addCards(2,"10", "Spades",     10, null);
+            make.addCards(3,"9", "Clubs",        9, null);
+            make.addCards(3,"9", "Hearts",       9, null);
+            make.addCards(3,"9", "Spades",       9, null);
+            make.addCards(4,"8", "Clubs", 8, null);
+            make.addCards(4,"8", "Diamonds", 8, null);
+            make.addCards(5,"7", "Diamonds", 7, null);
+            make.addCards(5,"7", "Hearts", 7, null);
+            make.addCards(4,"8", "Hearts", 8, null);
+            make.addCards(1,"Ace", "Spades", 11, 1);
+            make.addCards(4,"8", "Spades", 8, null);
+            make.addCards(5,"7", "Clubs", 7, null);
+            make.addCards(5,"7", "Spades", 7, null);
+            make.addCards(6,"6", "Clubs", 6, null);
+            make.addCards(6,"6", "Diamonds", 6, null);
+            make.addCards(6,"6", "Hearts", 6, null);
+            make.addCards(6,"6", "Spades", 6, null);
+            make.addCards(7,"5", "Clubs", 5, null);
+            make.addCards(7,"5", "Diamonds", 5, null);
+            make.addCards(7,"5", "Hearts", 5, null);
+            make.addCards(7,"5", "Spades", 5, null);
+            make.addCards(8,"4", "Clubs", 4, null);
+            make.addCards(8,"4", "Diamonds", 4, null);
+            make.addCards(8,"4", "Hearts", 4, null);
+            make.addCards(8,"4", "Spades", 4, null);
+            make.addCards(9,"3", "Clubs", 3, null);
+            make.addCards(9,"3", "Diamonds", 3, null);
+            make.addCards(9,"3", "Hearts", 3, null);
+            make.addCards(9,"3", "Spades", 3, null);
+            make.addCards(10,"2", "Clubs", 2, null);
+            make.addCards(10,"2", "Diamonds", 2, null);
+            
+            
+            int counter = make.deck.Count();
+            string cardTypeSecondCard = make.deck[1].Item2;
+            string suitSecondCard = make.deck[1].Item3;
+            string weightSecondCard = make.deck[1].Item4.ToString();
+            string altWeightSecondCard = make.deck[1].Item5.ToString();
 
-             Console.WriteLine("No args to fill the val, oh dispair.");
-             Console.ReadLine();
+            string cardTypeTwentiethCard = make.deck[19].Item2;
+            string suitTwentiethCard = make.deck[19].Item3;
+            string weightTwentiethCard = make.deck[19].Item4.ToString();
+            string altWeightTwentiethCard = make.deck[19].Item5.ToString();
+            
+            Console.WriteLine("Deck contains " + counter.ToString() + " cards.");
+            Console.WriteLine("Second card in deck is " + cardTypeSecondCard + " of " + suitSecondCard);
+            Console.WriteLine("Weight of second card in deck is " + weightSecondCard);
+            Console.WriteLine("Alternate weight of second card in deck is " + altWeightSecondCard);
+            Console.WriteLine("Card type of 20th card in deck is " + cardTypeTwentiethCard + " of " + suitTwentiethCard);
+            Console.WriteLine("Weight of 20th card in deck is " + weightTwentiethCard);
+            Console.WriteLine("Alternate weight of 20th card in deck is " + altWeightTwentiethCard);
+
+
+            make.deck.Sort();
+            //var sorter = from element in make.deck
+            //              orderby element.Item2, element.Item1
+            //              select element;
+
+            cardTypeSecondCard = make.deck[1].Item2;
+            suitSecondCard = make.deck[1].Item3;
+            Console.WriteLine("After sort, second card in deck is " + cardTypeSecondCard + " of " + suitSecondCard);
+            
+
+            make.deck.Remove(make.deck[0]);
+            counter = make.deck.Count();
+
+            Console.WriteLine("After dealing one card, deck contains " + counter.ToString() + " cards.");
+
+            Console.ReadLine();
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
-
-            
     }
-
 }
