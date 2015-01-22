aebfc01b8a244842d394ef14bae446f8880aa49a
ewascent@gmail.com <ewascent@gmail.com>
2015-01-21 23:25:21 -0800

    Final changes includes shuffle and sort logic.

---
+++ Helpers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace CardGame
+{
+    ///<summary>
+    /// Helper class to create a list that can store and sort a set of 5-tuples. 
+    /// This supports storing a set of records where each item in the reecord consists of heterogenous datatpes.
+    /// This allows an in memory table like structure with the ability to sort on a defined key. 
+    /// Typing and nullability is defined by the envoking class.
+    ///</summary>
+    public class TupleList<T1, T2, T3, T4, T5> : List<Tuple<T1, T2, T3, T4, T5>>
+    {
+        public void Add(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5)
+        {
+            Add(new Tuple<T1, T2, T3, T4, T5>(item1, item2, item3, item4, item5));
+        }
+    }
+
+
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic]
+        private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+      static class MyExtensions
+      {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+          int n = list.Count;
+          while (n > 1)
+          {
+            n--;
+            int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+          }
+        }
+      }
+ }
--- ManageDeck.cs
+++ ManageDeck.cs
@@ -3,37 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace CardGame
 
 {
-    //Create a helper class to simplify the creation of a list of tuples.
-    //Each card will be represented as a tuple and the list will represent the deck
-    public class TupleList<T1, T2, T3, T4, T5> : List<Tuple<T1, T2, T3, T4,T5>>
-    {
-        public void Add(T1 item1, T2 item2, T3 item3, T4 item4, T5 item5)
-        {
-            Add(new Tuple<T1, T2, T3, T4, T5>(item1, item2, item3, item4, item5));
-        }
-    }
+
+
+    /// <summary>
+    /// Class allows you to add cards to a sorted deck. The deck is represented as a set of tuples.
+    /// Suittype is a list used to validate you are using valid suits in your deck.
+    /// Card type is the same, but for cards.
+    /// </summary>
+
     public class ManageDeck
     {
         List<string> suitType = new List<string>();
         List<string> cardType = new List<string>();
         public TupleList<byte,string, string, byte, byte?> deck = new TupleList<byte,string, string, byte, byte?>();
-
-        //byte is an economical numeric datatype that stores values up to 32,767
-        //alternateWeight is usually null adn is deisgned for card games that allow one card to have two values like the ace in blackjack
-        public string addCards(byte sortOrder,string cardVal, string suitVal, byte weight, byte? alternateWeight)
+        RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+  
+        /// <summary>
+        ///byte is an economical numeric datatype that stores values up to 32,767
+        ///alternateWeight is usually null and is deisgned for card games that allow one card to have two values like the ace in blackjack
+        /// Class allows you to add cards to a sorted deck. The deck is represented as a set of tuples.
+        /// Suittype is a list used to validate you are using valid suits in your deck.
+        /// Card type is the same, but for cards.
+        /// </summary>
+        /// 
+        /// <param name="sortOrder">a byte value used as an index for sorting the deck in default order prior to shuffling</param>
+        /// <param name="cardVal">The value of a card you want to add. Must match cardType list.</param>
+        /// <param name="suitVal">The suit of a card you want to add. Must match suitType list.</param>
+        /// <param name="weight">Attribute of a card that indicates value relative to other cards. Customizable by game. A king and 10 are the same weight in black jack, but the king has a higher weight in poker.</param>
+        /// <param name="alternateWeight">In some games a card can have more than one value. In blackjack the Ace has a value of 11 or 1.</param>
+        /// <param name="sortDeck">boolean to indicate if deck should be sorted. Shuffling will superscede sorting if both are set to true.</param>
+        /// <param name="shuffleDeck">boolean to indicate if deck should be shuffled. Shuffling will superscede sorting if both are set to true.</param>
+        /// <example>
+        /// var game = new ManageDeck();
+        /// game.makeDeck(2, "2", "Clubs", 1, null,false,false);
+        /// game.makeDeck(3, "Ace", "Hearts", 11, 1);
+        /// game.makeDeck(1, "Ace", "Clubs", 11, 1);
+        /// </example>
+        public string makeDeck(byte sortOrder,string cardVal, string suitVal, byte weight, byte? alternateWeight, bool sortDeck, bool shuffleDeck)
         {
             var format = new ArgumentException("Use a conventional card and suit.");
 
             try
             {
+                //start adding valid suits to suitType list
                 suitType.Add("Clubs");
                 suitType.Add("Diamonds");
                 suitType.Add("Hearts");
                 suitType.Add("Spades");
+
+                //start adding valid suits to cardType list
                 cardType.Add("Ace");
                 cardType.Add("King");
                 cardType.Add("Queen");
@@ -47,12 +70,37 @@ namespace CardGame
                 cardType.Add("4");
                 cardType.Add("3");
                 cardType.Add("2");
+
+                //validate cards ad suits based on lsit values
                 if (cardType.Contains(cardVal))
                 {
+                    //add card and sort deck
                     if (suitType.Contains(suitVal))
                     {
                         deck.Add(sortOrder, cardVal, suitVal, weight, alternateWeight);
-                        return "Eric does not know how to return a tuplelist";
+                        
+                        if(sortDeck == true && shuffleDeck  == false)
+                        { 
+                            deck.Sort();
+                        }
+
+                        if (shuffleDeck  ==  true)
+                        {        
+                            int n = deck.Count;
+                            while (n > 1)
+                            {
+                                var box = new byte[1];
+                                do provider.GetBytes(box);
+                                while (!(box[0] < n * (Byte.MaxValue / n)));
+                                var k = (box[0] % n);
+                                n--;
+                                var value = deck[k];
+                                deck[k] =   deck[n];
+                                deck[n] = value;
+                            }  
+                        }
+
+                        return "Success";
                     }
                     else throw format;
                 }
@@ -61,76 +109,76 @@ namespace CardGame
             catch
             {
                 throw format;
-                //return "Exception 100: Card could not be added";
             }
         }
 
-        public void shuffleDeck(int CardCount)
-        {
-            Console.WriteLine("I am dead to the world. fix me");
-        }
-
+     /// <summary>
+     /// Really, a summary of the main method. Well it is an interview. This is just sample code used to deomostrat the deck and shuffling. 
+     /// </summary>
+     /// <param name="args">Unsed here, but feel free to throw some args in there.</param>
         private static void Main(string[] args)
         {
             var make = new ManageDeck();
 
-            make.addCards(10,"2", "Hearts", 2, null);
-            make.addCards(1,"Ace", "Clubs", 11, 1);
-            make.addCards(2,"10", "Clubs", 10, null);
-            make.addCards(3,"9", "Diamonds", 9, null);
-            make.addCards(2,"King", "Clubs", 10, null);
-            make.addCards(2,"King", "Diamonds", 10, null);
-            make.addCards(10, "2", "Spades", 2, null);
-            make.addCards(2, "King", "Hearts", 10, null);
-            make.addCards(2,"Queen", "Diamonds",10, null);
-            make.addCards(2,"Queen", "Hearts",  10, null);
-            make.addCards(2,"Queen", "Spades",  10, null);
-            make.addCards(6,"6", "Clubs", 6, null);
-            make.addCards(6,"6", "Diamonds", 6, null);
-            make.addCards(2,"King", "Spades", 10, null);
-            make.addCards(2,"Queen", "Clubs", 10, null);
-            make.addCards(2,"Jack", "Clubs", 10, null);
-            make.addCards(2,"Jack", "Diamonds", 10, null);
-            make.addCards(2,"Jack", "Hearts",   10, null);
-            make.addCards(1,"Ace", "Diamonds", 11, 1);
-            make.addCards(1,"Ace", "Hearts", 11, 1);
-            make.addCards(2,"Jack", "Spades", 10, null);
-            make.addCards(2,"10", "Diamonds",   10, null);
-            make.addCards(2,"10", "Hearts",     10, null);
-            make.addCards(2,"10", "Spades",     10, null);
-            make.addCards(3,"9", "Clubs",        9, null);
-            make.addCards(3,"9", "Hearts",       9, null);
-            make.addCards(3,"9", "Spades",       9, null);
-            make.addCards(4,"8", "Clubs", 8, null);
-            make.addCards(4,"8", "Diamonds", 8, null);
-            make.addCards(5,"7", "Diamonds", 7, null);
-            make.addCards(5,"7", "Hearts", 7, null);
-            make.addCards(4,"8", "Hearts", 8, null);
-            make.addCards(1,"Ace", "Spades", 11, 1);
-            make.addCards(4,"8", "Spades", 8, null);
-            make.addCards(5,"7", "Clubs", 7, null);
-            make.addCards(5,"7", "Spades", 7, null);
-            make.addCards(6,"6", "Clubs", 6, null);
-            make.addCards(6,"6", "Diamonds", 6, null);
-            make.addCards(6,"6", "Hearts", 6, null);
-            make.addCards(6,"6", "Spades", 6, null);
-            make.addCards(7,"5", "Clubs", 5, null);
-            make.addCards(7,"5", "Diamonds", 5, null);
-            make.addCards(7,"5", "Hearts", 5, null);
-            make.addCards(7,"5", "Spades", 5, null);
-            make.addCards(8,"4", "Clubs", 4, null);
-            make.addCards(8,"4", "Diamonds", 4, null);
-            make.addCards(8,"4", "Hearts", 4, null);
-            make.addCards(8,"4", "Spades", 4, null);
-            make.addCards(9,"3", "Clubs", 3, null);
-            make.addCards(9,"3", "Diamonds", 3, null);
-            make.addCards(9,"3", "Hearts", 3, null);
-            make.addCards(9,"3", "Spades", 3, null);
-            make.addCards(10,"2", "Clubs", 2, null);
-            make.addCards(10,"2", "Diamonds", 2, null);
-            
-            
+            make.makeDeck(10,"2", "Hearts", 2, null,false,false);
+            make.makeDeck(1,"Ace", "Clubs", 11, 1,false,false);
+            make.makeDeck(2,"10", "Clubs", 10, null,false,false);
+            make.makeDeck(3,"9", "Diamonds", 9, null,false,false);
+            make.makeDeck(2,"King", "Clubs", 10, null,false,false);
+            make.makeDeck(2,"King", "Diamonds", 10, null,false,false);
+            make.makeDeck(10, "2", "Spades", 2, null,false,false);
+            make.makeDeck(2, "King", "Hearts", 10, null,false,false);
+            make.makeDeck(2,"Queen", "Diamonds",10, null,false,false);
+            make.makeDeck(2,"King", "Spades", 10, null,false,false);
+            make.makeDeck(2,"Queen", "Clubs", 10, null,false,false);
+            make.makeDeck(2,"Jack", "Clubs", 10, null,false,false);
+            make.makeDeck(2,"Jack", "Diamonds", 10, null,false,false);
+            make.makeDeck(2,"Jack", "Hearts",   10, null,false,false);
+            make.makeDeck(1,"Ace", "Diamonds", 11, 1,false,false);
+            make.makeDeck(1,"Ace", "Hearts", 11, 1,false,false);
+            make.makeDeck(2,"Jack", "Spades", 10, null,false,false);
+            make.makeDeck(2,"10", "Diamonds",   10, null,false,false);
+            make.makeDeck(2,"10", "Hearts",     10, null,false,false);
+            make.makeDeck(2,"10", "Spades",     10, null,false,false);
+            make.makeDeck(3,"9", "Clubs",        9, null,false,false);
+            make.makeDeck(3,"9", "Hearts",       9, null,false,false);
+            make.makeDeck(3,"9", "Spades",       9, null,false,false);
+            make.makeDeck(4,"8", "Clubs", 8, null,false,false);
+            make.makeDeck(4,"8", "Diamonds", 8, null,false,false);
+            make.makeDeck(5,"7", "Diamonds", 7, null,false,false);
+            make.makeDeck(5,"7", "Hearts", 7, null,false,false);
+            make.makeDeck(4,"8", "Hearts", 8, null,false,false);
+            make.makeDeck(1,"Ace", "Spades", 11, 1,false,false);
+            make.makeDeck(4,"8", "Spades", 8, null,false,false);
+            make.makeDeck(5,"7", "Clubs", 7, null,false,false);
+            make.makeDeck(5,"7", "Spades", 7, null,false,false);
+            make.makeDeck(6,"6", "Clubs", 6, null,false,false);
+            make.makeDeck(6,"6", "Diamonds", 6, null,false,false);
+            make.makeDeck(2, "Queen", "Hearts", 10, null,false,false);
+            make.makeDeck(2, "Queen", "Spades", 10, null,false,false);
+            make.makeDeck(6, "6", "Hearts", 6, null,false,false);
+            make.makeDeck(6,"6", "Spades", 6, null,false,false);
+            make.makeDeck(7,"5", "Clubs", 5, null,false,false);
+            make.makeDeck(7,"5", "Diamonds", 5, null,false,false);
+            make.makeDeck(7,"5", "Hearts", 5, null,false,false);
+            make.makeDeck(7,"5", "Spades", 5, null,false,false);
+            make.makeDeck(8,"4", "Clubs", 4, null,false,false);
+            make.makeDeck(8,"4", "Diamonds", 4, null,false,false);
+            make.makeDeck(8,"4", "Hearts", 4, null,false,false);
+            make.makeDeck(8,"4", "Spades", 4, null,false,false);
+            make.makeDeck(9,"3", "Clubs", 3, null,false,false);
+            make.makeDeck(9,"3", "Diamonds", 3, null,false,false);
+            make.makeDeck(9,"3", "Hearts", 3, null,false,false);
+            make.makeDeck(9,"3", "Spades", 3, null,false,false);
+            make.makeDeck(10,"2", "Clubs", 2, null,false,false);
+            make.makeDeck(10,"2", "Diamonds", 2, null, true, false);
+
             int counter = make.deck.Count();
+
+            string cardTypefirstCard = make.deck[0].Item2;
+            string suitfirstCard = make.deck[0].Item3;
+            string weightfirstCard = make.deck[1].Item4.ToString();
+            string altWeightfirstCard = make.deck[1].Item5.ToString();
             string cardTypeSecondCard = make.deck[1].Item2;
             string suitSecondCard = make.deck[1].Item3;
             string weightSecondCard = make.deck[1].Item4.ToString();
@@ -142,32 +190,29 @@ namespace CardGame
             string altWeightTwentiethCard = make.deck[19].Item5.ToString();
             
             Console.WriteLine("Deck contains " + counter.ToString() + " cards.");
+            Console.WriteLine("First card in deck is " + cardTypefirstCard + " of " + suitfirstCard);
             Console.WriteLine("Second card in deck is " + cardTypeSecondCard + " of " + suitSecondCard);
-            Console.WriteLine("Weight of second card in deck is " + weightSecondCard);
-            Console.WriteLine("Alternate weight of second card in deck is " + altWeightSecondCard);
+            Console.WriteLine("Weight of first card in deck is " + weightfirstCard);
+            Console.WriteLine("Alternate weight of first card in deck is " + altWeightfirstCard);
             Console.WriteLine("Card type of 20th card in deck is " + cardTypeTwentiethCard + " of " + suitTwentiethCard);
             Console.WriteLine("Weight of 20th card in deck is " + weightTwentiethCard);
             Console.WriteLine("Alternate weight of 20th card in deck is " + altWeightTwentiethCard);
 
 
-            make.deck.Sort();
-            //var sorter = from element in make.deck
-            //              orderby element.Item2, element.Item1
-            //              select element;
-
-            cardTypeSecondCard = make.deck[1].Item2;
-            suitSecondCard = make.deck[1].Item3;
-            Console.WriteLine("After sort, second card in deck is " + cardTypeSecondCard + " of " + suitSecondCard);
-            
-
             make.deck.Remove(make.deck[0]);
             counter = make.deck.Count();
+            Console.WriteLine("Deck contains " + counter.ToString() + " cards after dealing one.");
 
-            Console.WriteLine("After dealing one card, deck contains " + counter.ToString() + " cards.");
-
+            make.makeDeck(10, "2", "Hearts", 2, null, false, true);
+            counter = make.deck.Count();
+            cardTypefirstCard = make.deck[0].Item2;
+            suitfirstCard = make.deck[0].Item3;
+            Console.WriteLine("Deck contains " + counter.ToString() + " cards after adding one and shuffling.");
+            Console.WriteLine("First card in deck is " + cardTypefirstCard + " of " + suitfirstCard);
+            
             Console.ReadLine();
             // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
+            Console.WriteLine("Press any key to exit."); 
             Console.ReadKey();
         }
     }
